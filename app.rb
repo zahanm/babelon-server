3d0de2c971264f9f47b0b77761fab171353cbe3f
Aaron Kalb <kalb@stanford.edu>
2012-02-21 22:52:08 -0800

    More testing.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 
-get '/english/' do
-    `bash test.sh #{params[:arg]}`
+get '/:source/:target/:sentence' do
+    `bash pause-test.sh #{params[:sentence]} #{params[:source]} #{params[:target]}`
+    #"Hello, #{params[:source]}, #{params[:target]}, #{params[:sentence]}"
     #"Hello, #{params[:name]}. #{params.inspect}"
 end
